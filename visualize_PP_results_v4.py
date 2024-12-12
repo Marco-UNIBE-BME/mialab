d6a01bac4bc86fc12cafa411f3247b9d2eac1250
José Inácio <joseinacio97@gmail.com>
2024-12-12 15:24:59 +0100

    Corrections of the visualizer

--- visualize_PP_results_v4.py
+++ visualize_PP_results_v4.py
@@ -148,7 +148,7 @@ class NiftiImageViewer:
     def update_display(self, initial=False):
         """Update the displayed slices for the current axis and slice."""
         if self.axis == 0:
-            slice1 = sitk.GetArrayViewFromImage(self.image_gt)[self.current_slice, :, :]
+            slice1 = np.fliplr(sitk.GetArrayViewFromImage(self.image_gt)[self.current_slice, :, :])
             slice2 = sitk.GetArrayViewFromImage(self.image_raw)[self.current_slice, :, :]
             slice3 = sitk.GetArrayViewFromImage(self.image_pp)[self.current_slice, :, :]
         elif self.axis == 1:
@@ -193,10 +193,10 @@ class NiftiImageViewer:
 
 
 if __name__ == "__main__":
-    filepath_gt = "dataset/test/117122/labels_native.nii.gz"  # Ground truth labels
-    filepath_raw = "mia-result/2024-11-18-19-46-59 (noPP, ne20_md50)/117122_SEG.mha"  # Raw segmentation
-    filepath_pp = "mia-result/2024-12-07-17-08-58 (PP, ddO&HF2D50)/validation/117122_SEG-PP.mha"  # Post-processed segmentation (validation)
-    #filepath_pp = "mia-result/2024-12-07-14-22-04 (PP-ddO&HF50)/testing/118528_SEG-PP.mha"  # Post-processed segmentation (testing)
+    filepath_gt = "dataset/test/118528/labels_native.nii.gz"  # Ground truth labels
+    filepath_raw = "mia-result/2024-11-18-19-46-59 (noPP, ne20_md50)/118528_SEG.mha"  # Raw segmentation
+    #filepath_pp = "mia-result/2024-12-09_ddOpening&2DHF/2024-12-07-17-08-58 (ddO&2DHF50)/validation/117122_SEG-PP.mha"  # Post-processed segmentation (validation)
+    filepath_pp = "mia-result/2024-12-09_ddOpening&2DHF/2024-12-07-17-08-58 (ddO&2DHF50)/testing/118528_SEG-PP.mha"  # Post-processed segmentation (testing)
 
     gt = sitk.ReadImage(filepath_gt)
     raw = sitk.ReadImage(filepath_raw)
