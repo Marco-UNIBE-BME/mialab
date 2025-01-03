e2daf3a64b740ec998b2fee008723725937d9f2a
Portmann Marco Daniel <marco.portmann@students.unibe.ch>
2025-01-03 17:06:58 +0100

    Removed loader.py and fillhole results

--- loader.py
+++
@@ -1,7 +0,0 @@
-import joblib  
-
-def load_pipeline_predictions(path_inference_output:str) -> tuple:
-    with open(path_inference_output, 'rb') as f:
-            (images_test, images_prediction, images_probabilities) = joblib.load(f)
-    
-    return (images_test, images_prediction, images_probabilities)
